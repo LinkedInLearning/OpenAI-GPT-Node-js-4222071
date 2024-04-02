6a2e075caa9ebe73f81ba8bd7b295008b8871e21
Sandra mada <sandramada@sandras-mbp.home>
2024-04-02 13:14:25 +0200

    02_06

--- fichiers-exercice/02/index.js
+++ fichiers-exercice/02/index.js
@@ -3,6 +3,8 @@ const OpenAI = require("openai");
 const readlineSync = require("readline-sync");
 
 // Open AI configuration
+const openai = new OpenAI();
+const messages = [{ role: "system", content: "tu es un correcteur d'orthographe et de grammaire. Tu dois corriger les erreurs d'orthographe et de grammaire" }]
 
 function getInput(promptMessage) {
   return readlineSync.question(promptMessage, {
@@ -14,19 +16,34 @@ async function main() {
   console.log("\n\n----------------------------------");
   console.log("          CHAT WITH AI 🤖   ");
   console.log("----------------------------------\n");
+  console.log("type 'x' to exit the program \n");
   runConversation();
 }
 
 async function runConversation() {
-  const input = getInput("You: ");
 
-  if (!!input) {
-    try {
-      // run conversation here
-      runConversation();
-    } catch (error) {
-      console.error(error);
+  while (true) {
+    const input = getInput("You: ");
+
+    if (!!input) {
+      if (input === "x") {
+        console.log("Goodbye!")
+        process.exit()
+      }
+      messages.push({"role": "user", "content": input})
+      try {
+        // run conversation here
+        const completion = await openai.chat.completions.create({
+          messages: messages,
+          model: "gpt-3.5-turbo",
+        });
+        console.log("Bot: " + completion.choices[0].message.content);
+
+      } catch (error) {
+        console.error(error);
+      }
     }
   }
+  
 }
 main();
